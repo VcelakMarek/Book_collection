cac1fe92e72fc561574630161f77188423b6c457
Marek Včelák <vcelak.marek@gmail.com>
2023-09-27 01:42:51 +0200

    add isAdmin variant

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -16,35 +16,52 @@ const Book = ({ book, isAdmin }: Props) => {
     DELETE(`v1/books/${book.id.toString()}`)
     // router.push("/v1/books/admin")
   }
-  return (
-    <Link
-      href={{
-        pathname: `/bookDetails_${book.id}`,
-        query: book,
-      }}
-      className="w-72 h-[480px] bg-[#7e88c3] flex justify-center rounded-3xl my-5"
-    >
-      <figure className="w-56 h-[345px] absolute">
-        <img
-          src={book.imageLink}
-          alt={book.title}
-          className="rounded-3xl w-[224px] h-[345px] relative top-8 left-0"
-        />
-        <figcaption className="relative top-10 text-center  m-3">
-          <h2>{book.title}</h2>
-          <h3>{book.author}</h3>
-        </figcaption>
-        {isAdmin && (
-          <div>
-            <Button variant="secondary">Update</Button>
-            <Button variant="delete" onClick={onDelete}>
-              Delete
-            </Button>
-          </div>
-        )}
-      </figure>
-    </Link>
-  )
+
+  if (isAdmin) {
+    return (
+      <Link
+        href={{
+          pathname: `/bookDetails_${book.id}`,
+          query: book,
+        }}
+        className="mb-4 flex p-4 w-11/12 items-center justify-around mx-auto rounded-lg border-[1.5px] border-transparent bg-white gap-7 my-7 drop-shadow hover:border-[1.5px] hover:border-[#7C5DFA] flex-wrap"
+      >
+        <h3 className="basis-2/12">
+          <span className="font-bold text-grey">#</span>
+          {book.id}
+        </h3>
+        <h2 className="basis-2/12">{book.title}</h2>
+        <h2 className="basis-2/12">{book.author}</h2>
+        <div className="flex basis-2/12 items-center gap-5">
+          <Button variant="secondary">Update</Button>
+          <Button variant="delete" onClick={onDelete}>
+            Delete
+          </Button>
+        </div>
+      </Link>
+    )
+  } else
+    return (
+      <Link
+        href={{
+          pathname: `/bookDetails_${book.id}`,
+          query: book,
+        }}
+        className="w-72 h-[480px] bg-[#7e88c3] flex justify-center rounded-3xl my-5"
+      >
+        <figure className="w-56 h-[345px] absolute">
+          <img
+            src={book.imageLink}
+            alt={book.title}
+            className="rounded-3xl w-[224px] h-[345px] relative top-8 left-0"
+          />
+          <figcaption className="relative top-10 text-center  m-3">
+            <h2>{book.title}</h2>
+            <h3>{book.author}</h3>
+          </figcaption>
+        </figure>
+      </Link>
+    )
 }
 
 export default Book
