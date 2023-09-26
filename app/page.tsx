9fc5922d74f4007442065d983fe82abd1fc67866
VcelakMarek <vcelak.marek@gmail.com>
2023-09-26 17:58:13 +0200

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import BookList from "components/BookList"
+import Button from "components/Button"
 
 export default function Home() {
   return (
-    <>
-      <BookList />
-      <p>test</p>
-    </>
+    <div className="w-full h-full bg-white-background">
+      <header className="h-12 flex flex-row justify-between content-center m-4">
+        <h1>Book Collection</h1>
+        <Button href="/admin">Admin</Button>
+      </header>
+      <div className="h-px w-screen bg-gray-300"></div>
+      <main className=" flex flex-row ">
+        <BookList />
+      </main>
+    </div>
   )
 }
