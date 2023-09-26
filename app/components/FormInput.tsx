63fc4443f9ffd2a6ad3e5b0a4acca955c44c4b41
VcelakMarek <vcelak.marek@gmail.com>
2023-09-26 16:24:41 +0200

    create FormInput.tsx

---
+++ app/components/FormInput.tsx
@@ -0,0 +1,44 @@
+import { Field } from "react-final-form"
+
+type Props = {
+  size?: "small" | "large"
+  inputName: string
+  id: string
+  inputType?: "text" | "number"
+}
+
+const inputSize: { [key: string]: string } = {
+  small: "w-[240px]",
+  large: "w-[504px]",
+}
+
+const FormInput = ({
+  size = "large",
+  inputName,
+  id,
+  inputType = "text",
+}: Props) => {
+  return (
+    <label htmlFor={id}>
+      <Field name={id}>
+        {({ input }) => (
+          <div>
+            <div className="flex justify-between">
+              <h2>{inputName}</h2>
+            </div>
+            <input
+              {...input}
+              type={inputType}
+              className={`${inputSize[size]} `}
+              onChange={(e) => {
+                input.onChange(e) //final-form's onChange
+              }}
+            />
+          </div>
+        )}
+      </Field>
+    </label>
+  )
+}
+
+export default FormInput
