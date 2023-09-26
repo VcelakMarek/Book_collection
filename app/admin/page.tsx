e007cba983fa20b33b69fddb70552c2c29dbc9a0
VcelakMarek <vcelak.marek@gmail.com>
2023-09-26 23:27:26 +0200

    import Header

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,14 +1,10 @@
 import BookList from "components/BookList"
-import Button from "components/Button"
+import Header from "components/Header"
 
 export default function Admin() {
   return (
     <div className="w-full h-full ">
-      <header className="h-12 flex flex-row justify-between content-center m-4">
-        <h1>Book Collection</h1>
-        <Button href="/admin/new">New</Button>
-      </header>
-      <div className="h-px w-screen bg-gray-300"></div>
+      <Header isAdmin />
       <main className="flex justify-center items-center flex-row w-full">
         <BookList isAdmin={true} />
       </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import BookList from "components/BookList"
-import Button from "components/Button"
+import Header from "components/Header"
 
 export default function Home() {
   return (
     <div className="w-full h-full bg-white-background">
-      <header className="h-12 flex flex-row justify-between content-center m-4">
-        <h1>Book Collection</h1>
-        <Button href="/admin">Admin</Button>
-      </header>
-      <div className="h-px w-screen bg-gray-300"></div>
+      <Header />
       <main className=" flex flex-row ">
         <BookList />
       </main>
