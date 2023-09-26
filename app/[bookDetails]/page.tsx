2f5628063c9ae3cfaf64e5dfbca160e5e42eebbf
VcelakMarek <vcelak.marek@gmail.com>
2023-09-27 00:59:30 +0200

    add bookDetails

--- app/[bookDetails]/page.tsx
+++ app/[bookDetails]/page.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Header from "components/Header"
 import { BookTypes } from "types/bookTypes"
 
 type Props = {
@@ -6,9 +7,37 @@ type Props = {
 }
 
 const BookDetails = ({ searchParams }: Props) => {
-  console.log("BookData:", searchParams)
-  console.log("test")
-  return <div>BookDetails</div>
+  const bookData = searchParams
+
+  return (
+    <>
+      <Header isButton={false} />
+      <main className="bg-white-background flex flex-wrap h-screen w-screen pt-10">
+        <section className="w-[320px] mx-auto">
+          <img
+            className="h-[499px] w-[323px]"
+            src={bookData.imageLink}
+            alt={bookData.title}
+          ></img>
+        </section>
+        <section className="m-10 w-3/5 flex flex-col gap-5">
+          <h1>{bookData.title}</h1>
+          <h2>{bookData.author}</h2>
+          <p>{bookData.description}</p>
+          <div className="flex gap-10 mt-28">
+            <div>
+              <h3>Year</h3>
+              <p>{bookData.year}</p>
+            </div>
+            <div>
+              <h3>Pages</h3>
+              <p>{bookData.pages}</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default BookDetails
