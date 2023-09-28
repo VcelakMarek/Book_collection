6ff3d3f68ae6ebb694043b73ab283b55ff6a6e22
Marek Včelák <vcelak.marek@gmail.com>
2023-09-29 01:34:40 +0200

    delete fetchBooksData.ts

--- app/scripts/fetchBooksData.ts
+++
@@ -1,7 +0,0 @@
-import { GET } from "scripts/fetch"
-
-export const fetchBooksData = async () => {
-  const response = await GET("v1/books")
-  console.log("GET Request Response:", response)
-  return response
-}
