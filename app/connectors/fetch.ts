52fbc368526f367f59ebfcc7f63fb66fadea2d71
Marek Včelák <vcelak.marek@gmail.com>
2023-09-29 01:30:38 +0200

    refactor code

---
+++ app/connectors/fetch.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosResponse } from "axios"
+import { BookType } from "types/bookTypes"
+
+const BASE_URL = "https://650fc0ea3ce5d181df5ca6fb.mockapi.io/api/"
+const api = axios.create()
+
+export const GET = async <T>(url: string): Promise<AxiosResponse<T>> => {
+  return axios({ method: "GET", baseURL: BASE_URL, url })
+}
+
+export const POST = async <T>(
+  url: string,
+  data: BookType,
+): Promise<AxiosResponse<T>> => {
+  return axios({ method: "POST", baseURL: BASE_URL, url, data })
+}
+
+export const PUT = async <T>(
+  url: string,
+  data: BookType,
+): Promise<AxiosResponse<T>> => {
+  return axios({ method: "PUT", baseURL: BASE_URL, url, data })
+}
+
+export const DELETE = async <T>(url: string): Promise<AxiosResponse<T>> => {
+  return axios({ method: "DELETE", baseURL: BASE_URL, url })
+}
