6763825d87d1bc1611889040d8e6ed0555f39a41
Marek Včelák <vcelak.marek@gmail.com>
2023-09-29 14:17:01 +0200

    create Error404.tsx

---
+++ app/components/Eror404.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Eror404 = () => {
+  return (
+    <h1 className="h-screen grid place-items-center">
+      Error 404: id not found
+    </h1>
+  )
+}
+
+export default Eror404
