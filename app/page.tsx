cecf9dc51294e7d2b0bfcc84d5315185ad4aab15
VcelakMarek <vcelak.marek@gmail.com>
2023-11-19 17:47:02 +0100

    add AdminLogin modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
+import AdminLogin from "components/AdminLogin"
 import BookList from "components/BookList"
 import Header from "components/Header"
 
-export default function Home() {
+type Props = {
+  searchParams: Record<string, string> | null | undefined
+}
+
+export default function Home({ searchParams }: Props) {
+  const showModal = searchParams?.modal
+
   return (
     <>
       <Header />
       <main>
         <BookList />
       </main>
+
+      {showModal && <AdminLogin />}
     </>
   )
 }
